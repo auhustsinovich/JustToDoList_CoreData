0356e795c8fb82b8f9d253fa0fa51fe06e20ae82
auhustsinovich <avgustinovich4@gmail.com>
2022-08-06 20:33:26 +0200

    Initial Commit

---
+++ JustToDoList_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustToDoList_CoreData
+//
+//  Created by Daniil Auhustsinovich on 6.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
